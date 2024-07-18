c3e9cf5ec34c9be80772f85956d8a6f766de0979
N00B0D3 <77ruanp@gmail.com>
2024-07-18 12:15:50 -0300

    pagination(ajustado)

--- curso-laravel-nodeStudio/resources/views/custom/pagination.blade.php
+++ curso-laravel-nodeStudio/resources/views/custom/pagination.blade.php
@@ -4,38 +4,35 @@
     @if ($paginator->onFirstPage())
         <li class="disabled"><i class="material-icons">chevron_left</i></li>
     @else
-        <li class="waves-effect"><a href="{{ $paginator->previousPageUrl() }}"><i class="material-icons">chevron_right</i></a></li>
+        <li class="waves-effect"><a href="{{ $paginator->previousPageUrl() }}"><i class="material-icons">chevron_left</i></a></li>
     @endif
 
     {{-- Pagination Elements --}}
     @foreach ($elements as $element)
-    {{-- "three dots" separator --}}
-    @if (is_string($element))
-        <li class="disabled">{{ $element }}</li>
-    @endif
+        {{-- "three dots" separator --}}
+        @if (is_string($element))
+            <li class="disabled">{{ $element }}</li>
+        @endif
 
-    {{-- Array of links --}}
-    @if (is_array($element))
-        @foreach ($elements as $page => $url)
-            @if ($page == $paginator->currentPage())
-                <li class="active">
-                    <a>{{ $page }}</a>
-                </li>
-            @else
-                <li class="waves-effect"><a href="{{ $url }}" class="page-link">{{ $page }}</a></li>
-            @endif
-        @endforeach
-    @endif
+        {{-- Array of links --}}
+        @if (is_array($element))
+            @foreach ($element as $page => $url)
+                @if ($page == $paginator->currentPage())
+                    <li class="active">
+                        <a>{{ $page }}</a>
+                    </li>
+                @else
+                    <li class="waves-effect"><a href="{{ $url }}" class="page-link">{{ $page }}</a></li>
+                @endif
+            @endforeach
+        @endif
     @endforeach
 
-    {{-- Next page link--}}
+    {{-- Next page link --}}
     @if ($paginator->hasMorePages())
         <li class="waves-effect"><a href="{{ $paginator->nextPageUrl() }}"><i class="material-icons">chevron_right</i></a></li>
     @else
-        <li class="disabled"><a href="{{ $paginator->nextPageUrl() }}"><i class="material-icons">chevron_right</i></a></li>
-        
+        <li class="disabled"><i class="material-icons">chevron_right</i></li>
     @endif
-
 </ul>
-    
 @endif
